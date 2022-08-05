e64cf193b9a769622f9e30427b84a4e5d38be6a4
Karen.Jebakany <karen.jebakany@niit.com>
2022-08-05 18:55:57 +0530

    added demo solution

---
+++ src/main/java/com/jap/customers/CustomerService.java
@@ -0,0 +1,15 @@
+package com.jap.customers;
+
+
+
+import java.util.List;
+
+public class CustomerService {
+
+    // This method will return the names of the customers in sorted alphabetical order.
+public List<String> getListOfCustomersSortedByName(List<Customer> customerList){
+    return null;
+}
+
+
+}
