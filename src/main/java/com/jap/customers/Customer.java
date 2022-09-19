00942b4620826862829713a4d6d88055687ff07e
Shubham <rajputshubham615@gmail.com>
2022-09-19 09:43:49 +0530

    reformatted the code and ran the test cases

--- src/main/java/com/jap/customers/Customer.java
+++ src/main/java/com/jap/customers/Customer.java
@@ -86,7 +86,6 @@ public class Customer implements Comparable<Customer> {
                 '}';
     }
 
-
     @Override
     public int compareTo(Customer o) {
         return String.CASE_INSENSITIVE_ORDER.compare(this.customerName, o.customerName);
--- src/main/java/com/jap/customers/CustomerService.java
+++ src/main/java/com/jap/customers/CustomerService.java
@@ -8,6 +8,13 @@ import java.util.List;
 public class CustomerService {
 
     // This method will return the names of the customers in sorted alphabetical order.
+
+    /**
+     * This function takes a list of customers, sorts them by name, and returns a list of their names.
+     *
+     * @param customerList A list of Customer objects
+     * @return A list of strings
+     */
     public List<String> getListOfCustomersSortedByName(List<Customer> customerList) {
         List<String> names = new ArrayList<>();
         Collections.sort(customerList);
